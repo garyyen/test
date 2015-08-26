196bf6f680fc87acf4de9e74a6b63c3a45fe3846
gary <gary@geegodeMacBook-Air-4.local>
2015-08-26 19:30:17 +0800

    test2

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding=utf-8
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Firefox()
+
+
+driver.get("https://www.google.com.tw")
